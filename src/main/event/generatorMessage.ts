3505f5f60f55a8277dfedea663e6f4dfe5e1a88f
grandkiwi219 <grandkiwi219@users.noreply.github.com>
2026-01-14 02:28:54 +0900

    Edit

--- src/main/event/generatorMessage.ts
+++ src/main/event/generatorMessage.ts
@@ -63,7 +63,6 @@ export function generatorMessage(event: MessageEvent) {
                 
                 default: return;
             }
-            return;
         }
 
         default: return;
